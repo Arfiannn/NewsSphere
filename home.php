bde1f1e888f80d6af505cc514bd50a55367ea879
Gdeliemena <162406489+Gdeliemena@users.noreply.github.com>
2024-06-05 00:10:07 +0800

    membuat halaman home

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>News App</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+</head>
+<body>
+  <div class="heading-container">
+    <h4>News Sphere</h4>
+  </div>
+  <div class="options-container"></div>
+  <div class="container"></div>
+</body>
+</html>
